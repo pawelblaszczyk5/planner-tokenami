047a3cff6da507d43ffc39824ce1328207c97700
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2023-12-27 21:29:56 +0100

    Add unplugin icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import optimizeLocales from "@react-aria/optimize-locales-plugin";
 import react from "@vitejs/plugin-react-swc";
+import Icons from "unplugin-icons/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -14,5 +15,14 @@ export default defineConfig({
 			}),
 			enforce: "pre",
 		},
+		Icons({
+			autoInstall: true,
+			compiler: "jsx",
+			iconCustomizer: (_collection, _icon, props) => {
+				props["aria-hidden"] = "true";
+				props["focusable"] = "false";
+			},
+			jsx: "react",
+		}),
 	],
 });
