bf432da28108e580556c5cae0c87b6195f2c41ab
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2024-01-13 12:44:02 +0100

    Add canary declaration for react-dom

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,3 +1,4 @@
 /* eslint-disable spaced-comment -- typescript reference broken by additional spaces */
 /// <reference types="vite/client" />
 /// <reference types="react/canary" />
+/// <reference types="react-dom/canary" />
